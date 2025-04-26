bea0982cc1c6dedc05540a31edc01d776c975f33
Krishna474-dev <vamsikrishnaallu474@gmail.com>
2025-04-26 17:29:56 +0530

    Fixed the normal vercel issue while using slug crash issue for slug routes

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 import classes from "./page.module.css";
 import Image from "next/image";
-import { getMeal } from "@/lib/meals";
+import { getMeal, getMeals } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
 export const metadata = {
@@ -10,14 +10,24 @@ export const metadata = {
   description: "Delicious meals, shared by a food-loving community.",
 };
 
-const MealDetails = ({ params }) => {
-  const meal = getMeal(params.slug);
+// 🚀 This will tell Next.js to pre-build all meal pages
+export async function generateStaticParams() {
+  const meals = await getMeals(); // use getMeals() here
+
+  return meals.map((meal) => ({
+    slug: meal.slug,
+  }));
+}
+
+const MealDetails = async ({ params }) => {
+  const meal = await getMeal(params.slug);
 
   if (!meal) {
     notFound();
   }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
       <header className={classes.header}>
